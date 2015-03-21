7ee18795d771e1d315b42d8cb2068b9b6a66f2c1
Jan Kossmann <kossmann.jan@gmail.com>
2015-03-21 13:22:09 +0100

    Import requests. Enable debug. Test api call to mycarcloud

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import sys
 sys.path.append(".env/lib/python2.7/site-packages")
 
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    payload = {
+        'access_token': '08beec989bccb333439ee3588583f19f02dd6b7e',
+        'asset': '357322040163096',
+        'filter': 'BEHAVE_ID'
+    }
+    r = requests.post('http://api.mycarcloud.de/resource.php', data=payload)
+    return r.text
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
